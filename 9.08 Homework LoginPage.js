4d9f2e5aec4da320642b717969aca5958156d98d
AlyonaGladych <41871016+AlyonaGladych@users.noreply.github.com>
2018-08-29 20:40:48 +0300

    Create 9.08 Homework LoginPage.js

---
+++ 9.08 Homework LoginPage.js
@@ -0,0 +1,20 @@
+let BasePage = require('./BasePage.js');
+
+class LoginPage extends BasePage {
+    constructor(username, password) {
+        super(username, password);
+        this.name = username;
+        this.passw = password;
+    }
+    login() {
+    console.log(`Enter username`);
+    console.log(`Enter password`);
+    let arr = [];
+    
+    for (let i =0; i < this.passw.length; i++) arr[i] = this.passw[i];
+
+    console.log(`You are logged in as ${this.name}, your password is ${arr.join("")}`);
+    }
+}
+
+module.exports = LoginPage;
