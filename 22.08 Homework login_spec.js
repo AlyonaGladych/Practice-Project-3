9b1f210a19b36149b1b23cfb7524e28c9dbeff3a
AlyonaGladych <41871016+AlyonaGladych@users.noreply.github.com>
2018-08-29 21:19:00 +0300

    Create 22.08 Homework login_spec.js

---
+++ 22.08 Homework login_spec.js
@@ -0,0 +1,19 @@
+let WebButton = require("./WebButton.js");
+
+describe('LogIn', function () {
+    let loginButton = new WebButton(element(by.css('.login-button')), "Login Button");
+    let signInButton = new WebButton(element(by.css('.iframe-wrap .login-button')), "Sign In Button");
+
+    it('User is able to login with valid credentials', async () => {
+        await browser.get('http://eds_university.eleks.com/login');
+        await loginButton.click();
+        await element(by.id('email')).sendKeys(browser.params.username);
+        await element(by.id('userPassword')).sendKeys(browser.params.password);
+        await signInButton.click();
+
+        let name = await element(by.css('.user-name')).getText();
+
+        expect(name).toEqual('Alyona Gladych');
+        console.log(`Username is ${name}`);
+    });
+});
