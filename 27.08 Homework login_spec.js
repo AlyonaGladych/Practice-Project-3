095d06c41332fd314162d2c66f220126efc7ee6a
AlyonaGladych <41871016+AlyonaGladych@users.noreply.github.com>
2018-08-29 21:37:16 +0300

    Create 27.08 Homework login_spec.js

---
+++ 27.08 Homework login_spec.js
@@ -0,0 +1,20 @@
+let LoginPage = require('./27.08 Homework PageObject.js');
+
+describe('login page', function() {
+    it('should be able to login with valid credentials', async function() {
+        await LoginPage.get();
+
+        await LoginPage.clickLogin();
+
+        await LoginPage.setName('alyonagladych@gmail.com');
+
+        await LoginPage.setPassword('x&b*zmzA^oj');
+
+        await LoginPage.clickSignin();
+
+        let name = await element(by.css('.user-name')).getText();
+		    console.log(`Username is ${name}`);
+
+        expect(name).toEqual('Alyona Gladych');
+    });
+}); 
